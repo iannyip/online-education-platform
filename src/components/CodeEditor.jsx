c566ca641c5569a825abf5a9c637aab9d17a79cb
iannyip <ianyip@nullspace.com.sg>
2021-04-28 12:48:51 +0800

    used useRef hook to extract editor contents

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,16 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
+import React, { useRef } from "react";
 import Editor from "@monaco-editor/react";
 
 export default function CodeEditor() {
+  const editorRef = useRef(null);
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor;
+  };
+
+  const getValue = () => {
+    alert(editorRef.current.getValue());
+  };
+
   return (
-    <Editor
-      height="300px"
-      defaultLanguage="javascript"
-      defaultValue="// Write your code here"
-      theme="vs-dark"
-      loading="🤗  Loading... 🤗"
-    />
+    <div>
+      <Editor
+        height="300px"
+        defaultLanguage="javascript"
+        defaultValue="// Write your code here"
+        theme="vs-dark"
+        loading="🤗  Loading... 🤗"
+        onMount={handleEditorDidMount}
+      />
+      <button onClick={getValue}>Show Value</button>
+    </div>
   );
 }
