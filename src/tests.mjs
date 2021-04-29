7cb7577b809593df616afb6ebbae9781594dfb57
iannyip <ianyip@nullspace.com.sg>
2021-04-30 01:18:14 +0800

    added new tests into test.mjs

--- src/tests.mjs
+++ src/tests.mjs
@@ -31,9 +31,76 @@ export default function tests() {
     }
   };
 
+  const comment = () => {
+    try {
+      let message = "This test is incomplete";
+      let success = false;
+      console.log("inside comment");
+      return [success, message];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const datatypeA = () => {
+    try {
+      let message = "This test is incomplete";
+      let success = false;
+      console.log("inside datatypeA");
+      return [success, message];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const simpleAddition = () => {
+    try {
+      let message = "This test is incomplete";
+      let success = false;
+      console.log("inside simpleAddition");
+      return [success, message];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const stringConcat = () => {
+    try {
+      let message = "This test is incomplete";
+      let success = false;
+      console.log("inside stringConcat");
+      return [success, message];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const addFunction = () => {
+    try {
+      let message = "This test is incomplete";
+      let success = false;
+      console.log("inside addFunction");
+      return [success, message];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const toUpperFunction = () => {
+    try {
+      let message = "This test is incomplete";
+      let success = false;
+      console.log("inside toUpperFunction");
+      return [success, message];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return {
     test,
     consolelog,
+    comment,
+    datatypeA,
+    simpleAddition,
+    stringConcat,
+    addFunction,
+    toUpperFunction,
   };
 }
 
