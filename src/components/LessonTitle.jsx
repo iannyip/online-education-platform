aef2fe02eb49b39b41df1546916c516613b27ac7
iannyip <ianyip@nullspace.com.sg>
2021-04-29 22:45:02 +0800

    question to ask akira

--- src/components/LessonTitle.jsx
+++ src/components/LessonTitle.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function LessonTitle({ currentLesson }) {
+  // To ask akira: why does this run twice?
   console.log(currentLesson.title);
   return (
     <div>
