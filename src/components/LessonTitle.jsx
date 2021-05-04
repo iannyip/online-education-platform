cc7094a050ad4229954df596859e30fc86b36d9e
iannyip <ianyip@nullspace.com.sg>
2021-05-05 01:51:02 +0800

    changed colour and font style of LessonTitle

--- src/components/LessonTitle.jsx
+++ src/components/LessonTitle.jsx
@@ -4,8 +4,8 @@ export default function LessonTitle({ currentLesson }) {
   // To ask akira: why does this run twice?
   return (
     <div>
-      <h1>
-        {currentLesson.id}: {currentLesson.title}
+      <h1 className="lead display-6">
+        {currentLesson.id}. {currentLesson.title}
       </h1>
     </div>
   );
