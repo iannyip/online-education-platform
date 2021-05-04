963687768c8669f2868ff4235235a4d926ba65ff
iannyip <ianyip@nullspace.com.sg>
2021-05-05 01:45:42 +0800

    styled home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,8 +10,8 @@ export default function HomePage({
     <div className="container mt-4">
       {/* SECTION 1 */}
 
-      <div className="row my-4">
-        <div className="col-sm-6">
+      <div className="row my-4 flex-md-row flex-column-reverse">
+        <div className="col-md-6 text-center text-md-start">
           <h1 className="display-5 mb-4">Learn JavaScript</h1>
           <p className="my-0 lead">Start from the basics</p>
           <p className="my-0 lead">Learn by doing</p>
@@ -25,7 +25,7 @@ export default function HomePage({
             Get Started
           </button>
         </div>
-        <div className="col-sm-6">
+        <div className="col-md-6 pb-4 d-flex justify-content-center justify-content-md-start">
           <img
             src="images/javascript_logo.png"
             alt="progress"
@@ -37,15 +37,15 @@ export default function HomePage({
 
       {/* SECTION 2 */}
 
-      <div className="row">
-        <div className="col-6">
+      <div className="row d-flex">
+        <div className="pb-4 col-md-6 d-flex justify-content-center">
           <img
             src="images/process_ppt.png"
             alt="javascript logo"
             className="w-50"
           />
         </div>
-        <div className="col-6">
+        <div className="col-md-6 text-center text-md-start">
           <h1 className="display-5 mb-4">Track your progress</h1>
           <p className="my-0 lead">Come back to where you left off</p>
           <p className="my-0 lead">Tracked progress is effective learning</p>
@@ -61,9 +61,9 @@ export default function HomePage({
 
       {/* SECTION 3 */}
 
-      <div className="row">
-        <div className="col-6">
-          <h1 className="display-5 mb-4">Ready to start learning?</h1>
+      <div className="row flex-md-row flex-column-reverse">
+        <div className="col-md-6 text-center text-md-start">
+          <h1 className="display-5 mb-4 ">Ready to start learning?</h1>
           <p className="my-0 lead">Unlock your full learning potential</p>
           <button
             className="btn btn-outline-secondary mt-4"
@@ -72,7 +72,7 @@ export default function HomePage({
             Sign up today
           </button>
         </div>
-        <div className="col-6">
+        <div className="col-md-6">
           <img
             src="images/rocket_gif.gif"
             alt="javascript logo"
