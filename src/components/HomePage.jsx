c8f02439e901d64e3432e09a35de0f2a4fec42b6
iannyip <ianyip@nullspace.com.sg>
2021-05-04 12:24:53 +0800

    simple view of home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,32 @@ import React from "react";
 export default function HomePage() {
   console.log("rendering home page");
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container mt-4">
+      <div className="row my-4">
+        <div className="col-6">
+          <h1 className="display-5 mb-4">Learn JavaScript</h1>
+          <p className="my-0 lead">Start from the basics</p>
+          <p className="my-0 lead">Learn by doing</p>
+          <p className="my-0 lead">Tons of fun</p>
+          <button className="btn btn-primary mt-4">Get Started</button>
+        </div>
+        <div className="col-6">Logo here</div>
+      </div>
+      <hr className="home-view-lines" />
+      <div className="row">
+        <div className="col-6">Image here</div>
+        <div className="col-6">
+          <h1 className="display-5 mb-4">Track your progress</h1>
+          <p className="my-0 lead">Come back to where you left off</p>
+          <p className="my-0 lead">Tracked progress is effective learning</p>
+          <button className="btn btn-primary mt-4">Login</button>
+        </div>
+      </div>
+      <hr className="home-view-lines" />
+      <div className="row">
+        <div className="col-6">Content here</div>
+        <div className="col-6">Logo here</div>
+      </div>
     </div>
   );
 }
