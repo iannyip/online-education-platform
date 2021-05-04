2e28a60d56781b209d1b9c82a8f2e50178eb8193
iannyip <ianyip@nullspace.com.sg>
2021-05-04 12:43:03 +0800

    added image. changed button styles

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import javascript_logo from "../../public/javascript_logo.png";
 
 export default function HomePage() {
   console.log("rendering home page");
@@ -10,9 +11,14 @@ export default function HomePage() {
           <p className="my-0 lead">Start from the basics</p>
           <p className="my-0 lead">Learn by doing</p>
           <p className="my-0 lead">Tons of fun</p>
-          <button className="btn btn-primary mt-4">Get Started</button>
+          <button className="btn btn-outline-secondary mt-4">
+            Get Started
+          </button>
         </div>
-        <div className="col-6">Logo here</div>
+        <div className="col-6">
+          <img src={"../../public/javascript_logo.png"} alt="javascript logo" />
+          {/* <img src={javascript_logo} alt="javascript logo" /> */}
+        </div>
       </div>
       <hr className="home-view-lines" />
       <div className="row">
@@ -21,7 +27,7 @@ export default function HomePage() {
           <h1 className="display-5 mb-4">Track your progress</h1>
           <p className="my-0 lead">Come back to where you left off</p>
           <p className="my-0 lead">Tracked progress is effective learning</p>
-          <button className="btn btn-primary mt-4">Login</button>
+          <button className="btn btn-outline-secondary mt-4">Login</button>
         </div>
       </div>
       <hr className="home-view-lines" />
