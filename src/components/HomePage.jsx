e16dddb65a45b05fb4282a47eaeed72f08ad0c05
iannyip <ianyip@nullspace.com.sg>
2021-05-04 16:57:17 +0800

    styled images, added disclaimer

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,8 +5,10 @@ export default function HomePage() {
   console.log("rendering home page");
   return (
     <div className="container mt-4">
+      {/* SECTION 1 */}
+
       <div className="row my-4">
-        <div className="col-6">
+        <div className="col-sm-6">
           <h1 className="display-5 mb-4">Learn JavaScript</h1>
           <p className="my-0 lead">Start from the basics</p>
           <p className="my-0 lead">Learn by doing</p>
@@ -15,14 +17,27 @@ export default function HomePage() {
             Get Started
           </button>
         </div>
-        <div className="col-6">
-          <img src={"../../public/javascript_logo.png"} alt="javascript logo" />
+        <div className="col-sm-6">
+          <img
+            src="images/javascript_logo.png"
+            alt="progress"
+            className="w-50"
+          />
           {/* <img src={javascript_logo} alt="javascript logo" /> */}
         </div>
       </div>
       <hr className="home-view-lines" />
+
+      {/* SECTION 2 */}
+
       <div className="row">
-        <div className="col-6">Image here</div>
+        <div className="col-6">
+          <img
+            src="images/process_ppt.png"
+            alt="javascript logo"
+            className="w-50"
+          />
+        </div>
         <div className="col-6">
           <h1 className="display-5 mb-4">Track your progress</h1>
           <p className="my-0 lead">Come back to where you left off</p>
@@ -31,9 +46,38 @@ export default function HomePage() {
         </div>
       </div>
       <hr className="home-view-lines" />
+
+      {/* SECTION 3 */}
+
       <div className="row">
-        <div className="col-6">Content here</div>
-        <div className="col-6">Logo here</div>
+        <div className="col-6">
+          <h1 className="display-5 mb-4">Ready to start learning?</h1>
+          <p className="my-0 lead">Unlock your full learning potential</p>
+          <button className="btn btn-outline-secondary mt-4">
+            Sign up today
+          </button>
+        </div>
+        <div className="col-6">
+          {/* <div style={(background-image = url("images/rocket_gif.gif")}></div> */}
+          <img
+            src="images/rocket_gif.gif"
+            alt="javascript logo"
+            className="w-50 rocket-style"
+          />
+        </div>
+      </div>
+
+      {/* SECTION 4 */}
+
+      <div className="row disclaimer-row">
+        <p>Disclaimer:</p>
+        <p>
+          This is a clone of{" "}
+          <a href="https://brilliant.org/" target="_blank">
+            brilliant.org
+          </a>{" "}
+        </p>
+        <p>Created for learning purposes</p>
       </div>
     </div>
   );
